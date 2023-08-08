4d7dfdeb7a507041045767c1b594e31029ef1e95
Perrie Russell <perussell0815@yahoo.com>
2023-08-08 15:20:42 +0000

    fix: authOption bug create new authOption ts file

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,5 @@
-import type { NextAuthOptions } from 'next-auth';
+import { authOptions } from '@lib/authOptions';
 import NextAuth from 'next-auth/next';
-import GithubProvider from 'next-auth/providers/github';
-
-export const authOptions: NextAuthOptions = {
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string,
-    }),
-  ],
-};
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
