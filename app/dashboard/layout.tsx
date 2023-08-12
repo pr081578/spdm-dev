3141eecdec032788abe3488157191aafed822927
Perrie Russell <perussell0815@yahoo.com>
2023-08-12 00:34:41 +0000

    fix: change body element to fragment to clear up a hydration error for dashboard layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -3,10 +3,10 @@ import React from 'react';
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <body>
+    <>
       <SidebarLightDashboard />
       {children}
-    </body>
+    </>
   );
 };
 
